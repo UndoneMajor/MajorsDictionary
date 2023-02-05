54fe8c53454df377715389e6f09105f5a7685694
UndoneMajor <124082595+UndoneMajor@users.noreply.github.com>
2023-02-05 18:29:15 +0800

    Breaking bad update

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 function search() {
-    let input = document.getElementById("searchTerm").value.toLowerCase();
-    let table = document.getElementById("dictionary");
-    let tr = table.getElementsByTagName("tr");
-    let noResults = document.getElementById("noResults");
-  
-    for (let i = 0; i < tr.length; i++) {
-      let td = tr[i].getElementsByTagName("td")[0];
+  let input = document.getElementById("searchTerm").value.toLowerCase();
+  let table = document.getElementById("dictionary");
+  let tr = table.getElementsByTagName("tr");
+  let noResults = document.getElementById("noResults");
+  let isMatched = false;
+
+  for (let i = 0; i < tr.length; i++) {
+    let tds = tr[i].getElementsByTagName("td");
+    let isRowMatched = false;
+
+    for (let j = 0; j < tds.length; j++) {
+      let td = tds[j];
       if (td) {
         let term = td.textContent || td.innerText;
         if (term.toLowerCase().indexOf(input) > -1) {
-          tr[i].style.display = "";
-          noResults.style.display = "none";
-        } else {
-          tr[i].style.display = "none";
-          noResults.innerHTML = "No results found for '" + input + "'";
-          noResults.style.display = "block";
+          isRowMatched = true;
+          isMatched = true;
+          break;
         }
-      } 
+      }
+    }
+
+    if (isRowMatched) {
+      tr[i].style.display = "";
+    } else {
+      tr[i].style.display = "none";
     }
   }
-  
+
+  if (!isMatched) {
+    noResults.innerHTML = "Хайлт олдсонгүй '" + input + "'";
+    noResults.style.display = "block";
+  } else {
+    noResults.style.display = "none";
+  }
+}
